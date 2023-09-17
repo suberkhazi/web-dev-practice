861ae79601ea52e1a21f0125f2a331bd251ff81e
suberkhazi <suberkhazi@gmail.com>
2023-09-17 12:58:28 +0530

    todo list displaying record

---
+++ js-practice/scripts/todo_list.js
@@ -0,0 +1,21 @@
+const todoList = ['abc', 'xyz']
+renderHtmlTodo();
+
+function renderHtmlTodo(){
+    let htmlTodo = '';
+for(let i=0; i<todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    htmlTodo += html;
+}
+console.log(htmlTodo);
+document.querySelector('.js-todo-list').innerHTML = htmlTodo;
+}
+
+function addTodo(){
+    const inputElement = document.querySelector('.js-todo-input')
+    const name = inputElement.value;
+    todoList.push(name)
+    inputElement.value = '';
+    renderHtmlTodo();
+}
